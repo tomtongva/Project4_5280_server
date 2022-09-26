3c914f13048e987acfaa0485124f19e8af444ca6
Tom Va <tomtongva@gmail.com>
2022-09-25 21:08:40 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express") //import express library
 const app = express() //app is an instance of express
 const port = 8080 //port number for application
+const jwt  = require("jsonwebtoken");
+const jwtSecret = "Group3KeyForJWT";
+const headerTokenKey = "x-jwt-token";
 
 app.listen(process.env.PORT || port, () => { //declare a web server
    		console.log(`Listening at http://localhost:${port}`) //careful with slanted single quote
@@ -13,4 +16,21 @@ app.get('/', (req, res) => { //can declare get our put route, first param is the
 app.post("/api/auth", async (req, res) => {
 
     res.send("Hello world post");
+});
+
+app.post("/api/auth", async (req, res) => {
+
+    let user = await findUser(req.body.email, req.body.password);
+    console.log("found " + user);
+    if (user) {
+        let token = jwt.sign({uid: user._id, name: user.firstName + " " + user.lastName, 
+                                exp:Math.floor(Date.now()/1000)+60*1,
+                                currentTime:Date.now()}, jwtSecret);
+        console.log("generating token for " + user.firstName);
+
+        res.send({email: user.email, firstName: user.firstName, lastName: user.lastName, gender: user.gender,
+            city: user.city, token: token, age: user.age, weight: user.weight, address: user.address});
+    } else {
+        res.status(401).send({error: "You're not found"});
+    }
 });
