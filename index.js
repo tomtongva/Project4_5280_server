b4205f55b197bab79dd47b6bf4fb361a56a2b6ff
Tom Va <tomtongva@gmail.com>
2022-09-26 17:13:48 -0400

    final

--- index.js
+++ index.js
@@ -5,30 +5,7 @@ const jwt  = require("jsonwebtoken");
 const jwtSecret = "Group3KeyForJWT";
 const headerTokenKey = "x-jwt-token";
 
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-
 app.listen(process.env.PORT || port, () => {
-    for (const name of Object.keys(nets)) {
-        for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-            if (net.family === familyV4Value && !net.internal) {
-                if (!results[name]) {
-                    results[name] = [];
-                }
-                results[name].push(net.address);
-            }
-        }
-    }
-    console.log(results);
-
-    //var os = require('os');
-    //console.log('Listening at http://' + os.hostname + ':${port}')
     console.log(`Listening on port ${port}`);
     console.log(`${process.env.PORT}`);
 });
